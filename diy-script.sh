ff796ecc2c16b1d5ef4a6df1eebc50c12ef1a90f
lejitao2013 <61481263+lejitao2013@users.noreply.github.com>
2024-12-15 10:32:58 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -4,6 +4,9 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/luci2/bin/config_generate
 
+# Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
