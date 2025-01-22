c3a9a97300cb80d8ab753f358f246bc6a160a110
lejitao2013 <61481263+lejitao2013@users.noreply.github.com>
2025-01-23 01:36:36 +0800

    修改默认ip

--- diy-script.sh
+++ diy-script.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/luci2/bin/config_generate
 
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
